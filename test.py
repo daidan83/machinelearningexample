d0bcf60baeb3bf3dbbbaf1bb69ba81aa7abce856
daidan83 <daidan83@hotmail.com>
2018-12-22 20:24:44 +0800

    1

--- test.py
+++ test.py
@@ -1,2 +1,63 @@
-import numpy as np 
+import numpy as np
 
+
+class NeuralNetwork():
+    def __init__(self):
+        # seeding for random number generation
+        np.random.seed(23234121)
+
+        # converting weights to a 3 by 1 matrix with values from -1 to 1 and mean of 0
+        self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
+
+    def sigmoid(self, x):
+        # applying the sigmoid function
+        return 1/(1+np.exp(-x))
+
+    def sigmoid_derivative(self, x):
+        # computing derivative to the sigmoid function
+        return x*(1-x)
+
+    def train(self, training_inputs, training_outputs, trainning_iterations):
+        output = self.think(training_inputs)
+
+        error = training_outputs - output
+
+        adjustments = np.dot(training_inputs.T, error *
+                             self.sigmoid_derivative(output))
+
+        self.synaptic_weights += adjustments
+
+    def think(self, inputs):
+        inputs = inputs.astype(float)
+        output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
+        return output
+
+
+if __name__ == "__main__":
+    neural_network = NeuralNetwork()
+
+    print("Beginning Randomly Generated Weights:")
+    print(neural_network.synaptic_weights)
+
+    training_inputs = np.array([[0, 0, 1],
+                                [1, 1, 1],
+                                [1, 0, 1],
+                                [0, 1, 1]])
+    training_outputs = np.array([[0, 1, 1, 0]]).T
+
+    neural_network.train(training_inputs, training_outputs, 1500000)
+
+    print("Ending Weights After Training:")
+    print(neural_network.synaptic_weights)
+
+    user_input_one = str(input("User Input One: "))
+    user_input_two = str(input("User Input Two: "))
+    user_input_three = str(input("User Input Three: "))
+
+    print("Considering New Situation: ", user_input_one,
+          user_input_two, user_input_three)
+    print("New Output data: ")
+    print(neural_network.think(
+        np.array([user_input_one, user_input_two, user_input_three])))
+
+print("Wow, we did it!")
