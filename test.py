048d8a9456c5b9341b84d33259444df5b440e5b4
daidan83 <daidan83@hotmail.com>
2018-12-22 21:46:45 +0800

    3

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 class NeuralNetwork():
     def __init__(self):
         # seeding for random number generation
-        np.random.seed(12)
+        #np.random.seed(12)
 
         # converting weights to a 3 by 1 matrix with values from -1 to 1 and mean of 0
         self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
@@ -17,15 +17,16 @@ class NeuralNetwork():
         # computing derivative to the sigmoid function
         return x*(1-x)
 
-    def train(self, training_inputs, training_outputs, trainning_iterations):
-        output = self.think(training_inputs)
+    def train(self, training_inputs, training_outputs, training_iterations):
+        for iteration in range(training_iterations):
+            output = self.think(training_inputs)
 
-        error = training_outputs - output
+            error = training_outputs - output
 
-        adjustments = np.dot(training_inputs.T, error *
-                             self.sigmoid_derivative(output))
+            adjustments = np.dot(training_inputs.T, error *
+                                self.sigmoid_derivative(output))
 
-        self.synaptic_weights += adjustments
+            self.synaptic_weights += adjustments
 
     def think(self, inputs):
         inputs = inputs.astype(float)
@@ -45,7 +46,7 @@ if __name__ == "__main__":
                                 [0, 1, 1]])
     training_outputs = np.array([[0, 1, 1, 0]]).T
 
-    neural_network.train(training_inputs, training_outputs, 1500000)
+    neural_network.train(training_inputs, training_outputs, 9999)
 
     print("Ending Weights After Training:")
     print(neural_network.synaptic_weights)
